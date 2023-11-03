9a4124992b014a6acd77cf4c936a8a621330abd0
Ethhat <roma.baranin.98@bk.ru>
2023-11-03 23:13:06 +0300

    Update maga.py

--- maga.py
+++ maga.py
@@ -1,2 +1,13 @@
-print("Hellow world")
-print("Hellow world")
+import tkinter as tk
+from random import randint, choice
+from tkinter import messagebox
+
+window = tk.Tk()
+
+window.resizable(width = False, height = False )
+
+window.title("Кнопки")
+
+window.geometry('720x360')
+
+window.mainloop()
